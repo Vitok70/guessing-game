e48ccaf06ef70b23c0d287647c1a2538de24ca38
Vitok70 <vitokunev70@gmail.com>
2022-08-13 16:57:47 +0300

    feat:task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor(number,minNum,maxNum) {
+        this.number = number; 
+        this.minNum = minNum;
+        this.maxNum = maxNum;
+    }
 
     setRange(min, max) {
-
+        this.minNum = min;
+        this.maxNum = max;
     }
 
     guess() {
-
+return Math.ceil((this.maxNum + this.minNum)/2);
     }
 
     lower() {
-
+        this.maxNum = Math.ceil((this.maxNum + this.minNum)/2);
     }
 
     greater() {
+        this.minNum = Math.ceil((this.maxNum + this.minNum)/2);
 
     }
 }
